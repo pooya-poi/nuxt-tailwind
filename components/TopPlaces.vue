dabed2ffee69675c275b083d5ecf88691ee58c47
pooya-poi <pooya585@gmail.com>
2021-06-14 00:43:20 +0430

    fix card margin in mobile view

--- components/TopPlaces.vue
+++ components/TopPlaces.vue
@@ -1,87 +1,62 @@
 <template>
   <div>
     <div class="bg-gray-300">
-      <div class="container mx-auto px-10 py-4 xs:text-xs ">
+      <div class="container mx-auto px-10 py-4 xs:text-xs">
         <h2 class="font-semibold">Popular destination</h2>
-        <p class="text-gray-600">
-          A selection of greate woek-friendly cities with lots to see and
-          explore.
-        </p>
+        <p class="text-gray-600">A selection of greate woek-friendly cities with lots to see and explore.</p>
       </div>
-      <div
-        class="
-          container
-          mx-auto
-          grid
-          sm:grid-cols-1
-          md:grid-cols-2
-          lg:grid-cols-3
-          gap-4
-          pb-20
-        "
-      >
-      
-        <card
-        v-for="city in cities" :key="city.id"
-          :img="city.img"
-          :city = "city.name"
-          :price="city.price"
-        />
-        
+      <div class="container mx-auto grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 pb-20">
+        <card class="mx-3" v-for="city in cities" :key="city.id" :img="city.img" :city="city.name" :price="city.price" />
       </div>
     </div>
   </div>
 </template>
 
 <script>
-
 // import Card from './Card.vue'
 export default {
   data() {
     return {
-      
       cities: [
         {
-          id:1,
+          id: 1,
           img: 'tehran.jpg',
           name: 'tehran',
-          price: 100
+          price: 100,
         },
         {
-          id:2,
+          id: 2,
           img: 'hamedan.jpg',
           name: 'hamedan',
-          price: 105
+          price: 105,
         },
         {
-          id:3,
+          id: 3,
           img: 'tabriz.jpg',
           name: 'tabriz',
-          price: 150
+          price: 150,
         },
         {
-          id:4,
+          id: 4,
           img: 'rasht.jpg',
           name: 'rasht',
-          price: 175
+          price: 175,
         },
         {
-          id:5,
+          id: 5,
           img: 'ahvaz.jpg',
           name: 'ahvaz',
-          price: 123
+          price: 123,
         },
         {
-          id:6,
+          id: 6,
           img: 'shiraz.jpg',
           name: 'shiraz',
-          price: 183
+          price: 183,
         },
       ],
     }
   },
-  
- 
 }
 </script>
 
