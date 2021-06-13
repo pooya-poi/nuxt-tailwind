066a42258b74b435f267e559bd92267848d752ac
pooya-poi <pooya585@gmail.com>
2021-06-13 14:56:10 +0430

    fix showing image in card

--- components/Card.vue
+++ components/Card.vue
@@ -1,113 +1,49 @@
 <template>
   <div>
-    <div
-      class="
-        flex
-        max-w-md
-        mx-auto
-        overflow-hidden
-        bg-white
-        rounded-lg
-        shadow-lg
-        dark:bg-gray-800
-      "
-    >
-      <!-- <div class="w-1/3 bg-cover" style="background-image: url('https://images.unsplash.com/photo-1494726161322-5360d4d0eeae?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80')"></div> -->
-      <!-- <div class="w-1/3 bg-cover" style="background-image: url('https://images.unsplash.com/photo-1448518340475-e3c680e9b4be?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=751&q=80')"></div> -->
-      <!-- <div
-        class="w-1/3 bg-cover"
-        :style="{ backgroundImage: `url('${img}')` }"
-      ></div> -->
-      <div
-        class="w-1/3 bg-cover"
-        :style="{ backgroundImage: `url('@/assets/img/city/${img}')` }"
-      ></div>
+    <div class="flex max-w-md mx-auto overflow-hidden bg-white rounded-lg shadow-lg dark:bg-gray-800">
+      <div class="w-1/3 bg-cover" :style="{ backgroundImage: `url(${require(`@/assets/img/city/${img}`)})` }"></div>
 
       <div class="w-2/3 p-4 md:p-4">
         <h1 class="text-2xl font-bold text-gray-800 dark:text-white">
           {{ city | capitalize }}
-          <!-- {{ capitalize(city) }} -->
         </h1>
 
-        <p class="mt-2 text-sm text-gray-600 dark:text-gray-400">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit In odit
-        </p>
-
+        <p class="mt-2 text-sm text-gray-600 dark:text-gray-400">Lorem ipsum dolor sit amet consectetur adipisicing elit In odit</p>
         <div class="flex mt-2 item-center">
-          <svg
-            class="w-5 h-5 text-gray-700 fill-current dark:text-gray-300"
-            viewBox="0 0 24 24"
-          >
-            <path
-              d="M12 17.27L18.18 21L16.54 13.97L22 9.24L14.81 8.63L12 2L9.19 8.63L2 9.24L7.46 13.97L5.82 21L12 17.27Z"
-            />
+          <svg class="w-5 h-5 text-gray-700 fill-current dark:text-gray-300" viewBox="0 0 24 24">
+            <path d="M12 17.27L18.18 21L16.54 13.97L22 9.24L14.81 8.63L12 2L9.19 8.63L2 9.24L7.46 13.97L5.82 21L12 17.27Z" />
           </svg>
 
-          <svg
-            class="w-5 h-5 text-gray-700 fill-current dark:text-gray-300"
-            viewBox="0 0 24 24"
-          >
-            <path
-              d="M12 17.27L18.18 21L16.54 13.97L22 9.24L14.81 8.63L12 2L9.19 8.63L2 9.24L7.46 13.97L5.82 21L12 17.27Z"
-            />
+          <svg class="w-5 h-5 text-gray-700 fill-current dark:text-gray-300" viewBox="0 0 24 24">
+            <path d="M12 17.27L18.18 21L16.54 13.97L22 9.24L14.81 8.63L12 2L9.19 8.63L2 9.24L7.46 13.97L5.82 21L12 17.27Z" />
           </svg>
 
-          <svg
-            class="w-5 h-5 text-gray-700 fill-current dark:text-gray-300"
-            viewBox="0 0 24 24"
-          >
-            <path
-              d="M12 17.27L18.18 21L16.54 13.97L22 9.24L14.81 8.63L12 2L9.19 8.63L2 9.24L7.46 13.97L5.82 21L12 17.27Z"
-            />
+          <svg class="w-5 h-5 text-gray-700 fill-current dark:text-gray-300" viewBox="0 0 24 24">
+            <path d="M12 17.27L18.18 21L16.54 13.97L22 9.24L14.81 8.63L12 2L9.19 8.63L2 9.24L7.46 13.97L5.82 21L12 17.27Z" />
           </svg>
 
           <svg class="w-5 h-5 text-gray-500 fill-current" viewBox="0 0 24 24">
-            <path
-              d="M12 17.27L18.18 21L16.54 13.97L22 9.24L14.81 8.63L12 2L9.19 8.63L2 9.24L7.46 13.97L5.82 21L12 17.27Z"
-            />
+            <path d="M12 17.27L18.18 21L16.54 13.97L22 9.24L14.81 8.63L12 2L9.19 8.63L2 9.24L7.46 13.97L5.82 21L12 17.27Z" />
           </svg>
 
           <svg class="w-5 h-5 text-gray-500 fill-current" viewBox="0 0 24 24">
-            <path
-              d="M12 17.27L18.18 21L16.54 13.97L22 9.24L14.81 8.63L12 2L9.19 8.63L2 9.24L7.46 13.97L5.82 21L12 17.27Z"
-            />
+            <path d="M12 17.27L18.18 21L16.54 13.97L22 9.24L14.81 8.63L12 2L9.19 8.63L2 9.24L7.46 13.97L5.82 21L12 17.27Z" />
           </svg>
         </div>
 
         <div class="flex justify-between mt-3 item-center">
-          <h1
-            class="
-              text-lg
-              font-bold
-              text-gray-700
-              dark:text-gray-200
-              md:text-xl
-            "
-          >
-            ${{ price }}
-          </h1>
-          <button
-            class="
-              px-2
-              py-1
-              text-xs
-              font-bold
-              text-white
-              uppercase
-              transition-colors
-              duration-200
-              transform
-              bg-gray-800
-              rounded
-              dark:bg-gray-700
-              hover:bg-gray-700
-              dark:hover:bg-gray-600
-              focus:outline-none
-              focus:bg-gray-700
-              dark:focus:bg-gray-600
-            "
-          >
-            Add to Cart
+          <h1 class="text-lg font-bold text-gray-700 dark:text-gray-200 md:text-xl">${{ price }}</h1>
+          <button class="px-2 py-1 text-xs font-bold text-white uppercase transition-colors duration-200 transform bg-gray-800 rounded dark:bg-gray-700 hover:bg-gray-700 dark:hover:bg-gray-600 focus:outline-none focus:bg-gray-700 dark:focus:bg-gray-600">
+            <span class="flex">
+              Add to Cart
+
+              <span class="">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
+                </svg>
+              </span>
+
+            </span>
           </button>
         </div>
       </div>
@@ -117,23 +53,22 @@
 
 <script>
 export default {
-  props:{
-    img:{
-      type:String,
-      required:true,
+  
+  props: {
+    img: {
+      type: String,
+      required: true,
     },
-    city:{
-      type:String,
-      required:true,
+    city: {
+      type: String,
+      required: true,
     },
-    price:{
-      type:Number,
-      required:true
-    }
-
-  }
+    price: {
+      type: Number,
+      required: true,
+    },
+  },
 }
 </script>
 
-<style lang="scss" scoped>
-</style>
+<style lang="scss" scoped></style>
