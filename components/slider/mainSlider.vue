a49a91c8e111e052136b7170c3293efdc00dc712
pooya-poi <pooya585@gmail.com>
2021-06-14 09:21:16 +0430

    adding swiper slider

---
+++ components/slider/mainSlider.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+	   <swiper
+    :slides-per-view="3"
+    :space-between="50"
+    @swiper="onSwiper"
+    @slideChange="onSlideChange"
+  >
+    <swiper-slide>Slide 1</swiper-slide>
+    <swiper-slide>Slide 2</swiper-slide>
+    <swiper-slide>Slide 3</swiper-slide>
+    ...
+  </swiper>
+  </div>
+</template>
+
+<script>
+// Import Swiper Vue.js components
+import { Swiper, SwiperSlide } from 'swiper/vue'
+
+// Import Swiper styles
+import 'swiper/swiper.scss'
+
+export default {
+	 components: {
+      Swiper,
+      SwiperSlide,
+    },
+	  methods: {
+      onSwiper(swiper) {
+        console.log(swiper);
+      },
+      onSlideChange() {
+        console.log('slide change');
+      },
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
--- pages/shop/index.vue
+++ pages/shop/index.vue
@@ -1,11 +1,14 @@
 <template>
 	<div>
-
+shop page
+<main-slider
 	</div>
 </template>
 
 <script>
+import mainSlider from '~/components/slider/mainSlider.vue'
 	export default {
+  components: { mainSlider },
 		
 	}
 </script>
