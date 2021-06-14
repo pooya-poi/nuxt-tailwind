fdac227c9ad1f4a39e85ffe91c5109f8d88a813f
pooya-poi <pooya585@gmail.com>
2021-06-14 19:38:36 +0430

    fixing card

--- components/shop/shopCard.vue
+++ components/shop/shopCard.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="bg-white rounded-lg shadow-lg">
-    <img src="@/assets/img/about.png" alt="" />
+  <div class="bg-white rounded-lg hover:shadow-xl transition transform hover:-translate-y-1 ease-linear duration-100 ">
+    <img class="rounded-tl-lg rounded-tr-lg" src="@/assets/img/about.png" alt="" />
     <h2 class="text-lg font-bold text-gray-700 dark:text-gray-200 md:text-xl mt-4">title</h2>
     <div class="flex justify-between mx-3 mt-8 pb-3">
       <span class="text-lg font-bold text-gray-700 dark:text-gray-200 md:text-xl">$1000.00</span>
