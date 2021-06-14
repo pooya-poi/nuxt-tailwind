a6a873ffe23e5f7d41ec95a677dda7c837898ddc
pooya-poi <pooya585@gmail.com>
2021-06-14 18:12:53 +0430

    shop page layouts

--- pages/shop/index.vue
+++ pages/shop/index.vue
@@ -1,12 +1,8 @@
 <template>
   <div>
-    <div class="container sm:mx-auto justify-center lg:flex mt-10">
-      <!-- <div class="lg:flex  w-1/4 ">
-        <div class="max-w-xs mx-auto overflow-hidden bg-white rounded-lg shadow-lg dark:bg-gray-800">
-          <img class="object-cover w-full h-56" src="https://images.unsplash.com/photo-1542156822-6924d1a71ace?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" alt="avatar" />
-        </div>
-        <main-slider class="w-3/4"></main-slider>
-      </div> -->
+    <!-- <div class="container sm:mx-auto justify-center lg:flex mt-10"> -->
+    <!-- <div class="grid lg:grid-cols-2 2xl:grid-cols-5">
+      
       <div class="border-black border-solid border-2 w-3/4 h-52"></div>
       <br class="mx-3" />
       <div class="border-black border-solid border-2 w-1/4 h-52 flex flex-col px-1 py-1">
@@ -14,7 +10,22 @@
 		  <br class="mb-1">
 	  	<div class="border-yellow-400 border-solid border-2 w-full h-1/2"></div>
 	  </div>
+    </div> -->
+
+    <div class="container mx-auto lg:grid lg:grid-cols-3 md:grid-cols-2 gap-10 ">
+
+
+
+      <div class="hidden h-48 lg:block border-black border-solid border-2 ">left - Bottom</div>
+      <div class="sm:mx-auto h-48 lg:mx-0 border-black border-solid border-2 col-span-2">right - Top</div>
+
+
+	  
     </div>
+
+
+
+
   </div>
 </template>
 
