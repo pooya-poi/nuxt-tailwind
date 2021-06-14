7c71723201b16eacc4ecebb88c2d5c846de74301
pooya-poi <pooya585@gmail.com>
2021-06-14 20:06:18 +0430

    fix shop cards component title not bold and fix hover effect

--- components/shop/shopCard.vue
+++ components/shop/shopCard.vue
@@ -1,16 +1,31 @@
 <template>
   <div class="bg-white rounded-lg hover:shadow-xl transition transform hover:-translate-y-1 ease-linear duration-100 ">
-    <img class="rounded-tl-lg rounded-tr-lg" src="@/assets/img/about.png" alt="" />
-    <h2 class="text-lg font-bold text-gray-700 dark:text-gray-200 md:text-xl mt-4">title</h2>
+    <img class="rounded-tl-lg rounded-tr-lg" :src="`${img}`" alt="" />
+    <h2 class="text-lg text-gray-700 dark:text-gray-200 md:text-xl mt-4 mx-1">{{title}}</h2>
     <div class="flex justify-between mx-3 mt-8 pb-3">
-      <span class="text-lg font-bold text-gray-700 dark:text-gray-200 md:text-xl">$1000.00</span>
+      <span class="text-lg font-bold text-gray-700 dark:text-gray-200 md:text-xl">${{price}}</span>
       <add-to-cart-btn></add-to-cart-btn>
     </div>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+	props:{
+		price:{
+			required:true,
+			type:Number
+		},
+		title:{
+			required:true,
+			type:String
+		},
+		img:{
+			required:true,
+			type:String
+		}
+	}
+}
 </script>
 
 <style lang="scss" scoped>
