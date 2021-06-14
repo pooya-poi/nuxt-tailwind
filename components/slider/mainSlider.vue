0d9229f55a06751b6f1627841a9cf197b08a52a7
pooya-poi <pooya585@gmail.com>
2021-06-14 13:37:46 +0430

    adding slider component and add it to shop page

--- components/slider/mainSlider.vue
+++ components/slider/mainSlider.vue
@@ -1,11 +1,42 @@
 <template>
   <div>
-    <VueSlickCarousel v-bind="settings" class="slider">
-      <div><img class="w-60" src="https://source.unsplash.com/1600x900/?nature,water" alt="" /></div>
-      <div><img class="w-60" src="https://source.unsplash.com/1600x901/?nature,water" alt="" /></div>
-      <div><img class="w-60" src="https://source.unsplash.com/1600x902/?nature,water" alt="" /></div>
-      <div><img class="w-60" src="https://source.unsplash.com/1600x903/?nature,water" alt="" /></div>
+    <VueSlickCarousel v-bind="settings" class="container slider lg:flex lg:justify-center">
+      <!-- <div><img class="" src="https://source.unsplash.com/1600x900/?nature,water" alt="" /></div>
+      <div><img class="" src="https://source.unsplash.com/1600x901/?nature,water" alt="" /></div>
+      <div><img class="" src="https://source.unsplash.com/1600x902/?nature,water" alt="" /></div>
+      <div><img class="" src="https://source.unsplash.com/1600x903/?nature,water" alt="" /></div> -->
+      <div class="bg-gray-700 text-white text-center h-40"><h1>1</h1></div>
+      <div class="bg-gray-700 text-white text-center h-40"><h1>2</h1></div>
+      <div class="bg-gray-700 text-white text-center h-40"><h1>3</h1></div>
+      <div class="bg-gray-700 text-white text-center h-40"><h1>4</h1></div>
+      <template #prevArrow="arrowOption">
+      <div class="custom-arrow">
+        {{ arrowOption.currentSlide }}/{{ arrowOption.slideCount }}
+      </div>
+    </template>
+      <template #nextArrow="arrowOption">
+      <div class="custom-arrow">
+        {{ arrowOption.currentSlide }}/{{ arrowOption.slideCount }}
+      </div>
+    </template>
     </VueSlickCarousel>
+<br>
+    <!-- <VueSlickCarousel :arrows="true" @init="onInitCarousel">
+      <div class="bg-gray-700 text-white text-center h-40"><h1>1</h1></div>
+      <div class="bg-gray-700 text-white text-center h-40"><h1>2</h1></div>
+      <div class="bg-gray-700 text-white text-center h-40"><h1>3</h1></div>
+      <div class="bg-gray-700 text-white text-center h-40"><h1>4</h1></div>
+      <template #prevArrow="arrowOption">
+        <div class="custom-arrow">{{ arrowOption.currentSlide }}/{{ arrowOption.slideCount }}</div>
+      </template>
+      /*...*/
+      <template #customPaging="page">
+        <div class="custom-dot">
+          {{ page }}
+        </div>
+      </template>
+    </VueSlickCarousel> -->
+
   </div>
 </template>
 
@@ -19,22 +50,43 @@ export default {
   data() {
     return {
       settings: {
-     "dots": true,
-  "focusOnSelect": true,
-  "infinite": true,
-  "speed": 500,
-  "slidesToShow": 3,
-  "slidesToScroll": 3,
-  "touchThreshold": 5
+        arrows:true,
+        dots: true,
+        dotsClass: 'slick-dots custom-dot-class',
+        edgeFriction: 0.35,
+        infinite: false,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
       },
     }
   },
   components: { VueSlickCarousel },
+  methods: {
+    showNext() {
+      this.$refs.carousel.next()
+    },
+  },
 }
 </script>
 
 <style lang="css" scoped>
-.slider{
+.slider {
   border: 2px red solid;
+}
+/* .slick-arrow.slick-prev.slick-disabled{
+  background: #000000ad !important;
+color: black !important;
+} */
+
+.custom-arrow.slick-arrow.slick-prev {
+    z-index: 99;
+}
+
+.slick-arrow.slick-prev {
+  left: 0;
+}
+.slick-arrow.slick-next {
+  right: 0;
 }
 </style>
--- pages/shop/index.vue
+++ pages/shop/index.vue
@@ -1,21 +1,46 @@
 <template>
-	<div>
-shop page
-<div class=" flex content-center">
+  <div>
+    shop page
+    <div class="container mx-auto">
+		<div v-for="img in sliderImage" :key="img.id">
+			<p>
+			{{ img.txt }}
 
-<main-slider></main-slider>
-</div>
-	</div>
+			</p>
+		</div>
+      <!-- <main-slider></main-slider> -->
+    </div>
+  </div>
 </template>
 
 <script>
 import mainSlider from '~/components/slider/mainSlider.vue'
-	export default {
-  components: { mainSlider },
+export default {
+  data() {
+    return {
 		
-	}
+      sliderImage: [
+        {
+          id: 1,
+          src: 'https://source.unsplash.com/1600x901/?nature,water',
+		  txt:'Lorem ipsum dolor sit amet.'
+        },
+        {
+          id: 2,
+          src: 'https://source.unsplash.com/1600x900/?nature,water',
+		  txt:'Lorem ipsum dolor sit amet.'
+        },
+        {
+          id: 3,
+          src: 'https://source.unsplash.com/1600x902/?nature,water',
+		  txt:'Lorem ipsum dolor sit amet.'
+        },
+      ],
+    }
+  },
+  components: { mainSlider },
+}
 </script>
 
 <style lang="scss" scoped>
-
 </style>
